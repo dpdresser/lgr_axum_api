92fd747a3a2e2602a272faa1c9f66143d99a418b
Dan Dresser <ddresser05@gmail.com>
2025-03-09 09:46:45 -0500

    Stage 1, Steps 1 and 2 setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{delete, get, post},
+    Router,
+};
+use std::net::SocketAddr;
+
+mod handlers;
+mod models;
+use handlers::*;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/question", post(create_question))
+        .route("/questions", get(read_questions))
+        .route("/question", delete(delete_question))
+        .route("/answer", post(create_answer))
+        .route("/answers", get(read_answers))
+        .route("/answer", delete(delete_answer));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
